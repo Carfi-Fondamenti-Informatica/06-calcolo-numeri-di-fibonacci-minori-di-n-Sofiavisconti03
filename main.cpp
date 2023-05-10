e77223c63415666e2e94db9d8994f91eecdd7530
Sofiavisconti03 <128122122+Sofiavisconti03@users.noreply.github.com>
2023-05-10 18:57:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     cout<<"inserire un numero intero n"<<endl;
     cin>>n;
     int c=0,a=1,b=1;
-    if(n<=2){
+    if(n>=2){
         cout<<a<<endl;
         cout<<b<<endl;
         while(c<(n-2)){
